dad834cf2c12b983ba1b8cfbd5aa795e9da08fe8
Lachi-Amine <ma.lachi@esi-sba.dz>
2024-01-16 13:53:28 +0100

    d
    d

--- script.js
+++ script.js
@@ -1,31 +1,37 @@
-let s=0;
-let m;
-let h;
+let s = 0;
+let m = 0;
+let h = 0;
 let j;
 function start() {
-    document.getElementById("start").disabled = true;
-        j= setInterval(() => {
-        s=s+1;
-         m=Math.trunc(s/60);
-         h=Math.trunc(m/60);
-    document.getElementById("hour").innerHTML=h;
-    document.getElementById("minute").innerHTML=m;
-    document.getElementById("second").innerHTML=s;
-    }, 1000);
+  document.getElementById("start").disabled = true;
+  j = setInterval(() => {
+    s = s + 1;
+    if (s == 60) {
+      s = 0;
+      m = m + 1;
+    }
+    if (m == 60) {
+      m = 0;
+      h = h + 1;
+    }
+    document.getElementById("hour").innerHTML = h;
+    document.getElementById("minute").innerHTML = m;
+    document.getElementById("second").innerHTML = s;
+  }, 1000);
 }
 
 function stop() {
-    clearInterval(j);
-    document.getElementById("start").disabled = false;
+  clearInterval(j);
+  document.getElementById("start").disabled = false;
 }
 function reset() {
-    clearInterval(j);
-    s=0;
-    m=0;
-    h=0;
-    document.getElementById("hour").innerHTML=h;
-    document.getElementById("minute").innerHTML=m;
-    document.getElementById("second").innerHTML=s;
-    document.getElementById("start").disabled = false;
-    start();    
+  clearInterval(j);
+  s = 0;
+  m = 0;
+  h = 0;
+  document.getElementById("hour").innerHTML = h;
+  document.getElementById("minute").innerHTML = m;
+  document.getElementById("second").innerHTML = s;
+  document.getElementById("start").disabled = false;
+  start();
 }
